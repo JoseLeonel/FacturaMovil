60fcd9ff74f906e13d0b7161cca09e3c39582373
JoseLeonel <josehernandezchaverri@gmail.com>
2018-12-06 18:00:32 -0600

    nada toque

--- src/main/java/com/emprendesoftcr/web/Controller/FacturasController.java
+++ src/main/java/com/emprendesoftcr/web/Controller/FacturasController.java
@@ -1053,7 +1053,7 @@ public class FacturasController {
 			Factura facturaBD = facturaBo.findById(idFactura);
 			
 
-			// Usuario usuario = usuarioBo.buscar(request.getUserPrincipal().getName());
+			 //Usuario usuario = usuarioBo.buscar(request.getUserPrincipal().getName());
 
 			// Se ejecuta este comando pero antes se ejecutan el comando para sacar la llave
 			// criptografica desde linux
