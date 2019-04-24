1f8ddb869942fe1ca02b3c35856f5f9679808bc2
JoseLeonel <josehernandezchaverri@gmail.com>
2019-04-24 13:25:24 -0600

    mejoras

--- src/main/java/com/emprendesoftcr/service/impl/ProcesoHaciendaServiceImpl.java
+++ src/main/java/com/emprendesoftcr/service/impl/ProcesoHaciendaServiceImpl.java
@@ -946,7 +946,7 @@ public class ProcesoHaciendaServiceImpl implements ProcesoHaciendaService {
 	 * Firmado de documentos
 	 * @see com.emprendesoftcr.service.ProcesoHaciendaService#procesoFirmado()
 	 */
-//	@Scheduled(cron = "0 0/10 * * * ?")
+	@Scheduled(cron = "0 0/10 * * * ?")
 	@Override
 	public synchronized void procesoFirmado() throws Exception {
 		try {
