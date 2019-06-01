1ce8acad8a10fa05a7fa9e7ecd6f05e9b3d5ef83
JoseLeonel <josehernandezchaverri@gmail.com>
2019-05-31 23:50:16 -0600

    mejoras

--- src/main/java/com/emprendesoftcr/Bo/Impl/CertificadoBoImpl.java
+++ src/main/java/com/emprendesoftcr/Bo/Impl/CertificadoBoImpl.java
@@ -39,8 +39,8 @@ public class CertificadoBoImpl implements CertificadoBo {
 			// desarrollo
 			// ClassPathResource cpr = new ClassPathResource("/home/jose/Escritorio/llaves/" + nombrep12);
 
-			nombrep12 = "011001046701.p12";
-			password = "0467";
+			nombrep12 = "010935016422.p12";
+			password = "0164";
 			ClassPathResource cpr = new ClassPathResource("/home/jose/dev/llaves/produccion/" + nombrep12);
 			String path = new ClassPathResource("/home/jose/dev/llaves/tmp/").getPath();
 			certificateInfo = P12Utils.dataFromP12(path, cpr.getPath(), password);
