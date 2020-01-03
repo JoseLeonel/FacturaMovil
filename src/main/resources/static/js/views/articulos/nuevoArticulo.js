1dcc486dea83dd9c7cd8326620acfdd22c3bd346
JoseLeonel <josehernandezchaverri@gmail.com>
2020-01-03 01:13:52 -0600

    mejoras

---
+++ src/main/resources/static/js/views/articulos/nuevoArticulo.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	_Init();
+} );/*fin document*/
+
+var _Init = function () {
+   __MantenimientoAgregar();
+}
+/**
+* Mostrar formulario de mantenimiento Agregar
+**/
+function __MantenimientoAgregar(){
+  	riot.compile(function() {
+			var parametros = {
+					tipoEjecucion:1
+			};
+     	  // here tags are compiled and riot.mount works synchronously
+		  var tags = riot.mount('articulo-crud',{parametros:parametros})
+  
+		});
+  
+}
+
+function __mostrarListado (){}
