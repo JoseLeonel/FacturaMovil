af9832821cf93d97972f435e3f5eb271502b5eda
josehernandezchaverri@gmail.com <josehernandezchaverri@gmail.com>
2020-10-13 20:23:59 -0600

    mejoras

--- src/main/java/com/emprendesoftcr/Utils/Constantes.java
+++ src/main/java/com/emprendesoftcr/Utils/Constantes.java
@@ -173,7 +173,7 @@ public class Constantes {
 	public static final Integer					LIBRE_IMPUESTOS_INACTIVO																			= 0;
 
 	// Semaforo
-	public static final Integer					BLOQUES_DOCUMENTOS_A_PROCESAR																	= 100;
+	public static final Integer					BLOQUES_DOCUMENTOS_A_PROCESAR																	= 30;
 	public static final Integer					BLOQUES_DOCUMENTOS_A_PROCESAR_CORREOS													= 30;
 
 	
