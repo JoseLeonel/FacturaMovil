37e39c0827025926fe10acb3cbf07ef36b559bdf
josehernandezchaverri@gmail.com <josehernandezchaverri@gmail.com>
2020-12-03 23:11:06 -0600

    mejoras

--- src/main/java/com/emprendesoftcr/service/impl/ProcesoHaciendaServiceImpl.java
+++ src/main/java/com/emprendesoftcr/service/impl/ProcesoHaciendaServiceImpl.java
@@ -742,7 +742,7 @@ public class ProcesoHaciendaServiceImpl implements ProcesoHaciendaService {
 					// recepcion.setCallbackUrl(Constantes.URL_JACODOS_CALLBACK);
 
 					// Jaco
-					// recepcion.setCallbackUrl(Constantes.URL_JACO_CALLBACK);
+					 recepcion.setCallbackUrl(Constantes.URL_JACO_CALLBACK);
 
 					// Inventario
 					// recepcion.setCallbackUrl(Constantes.URL_INVENTARIO_CALLBACK);
