9846e204bbe8c35d7394e07b8f20c9dfbbf0cb0f
josehernandezchaverri@gmail.com <josehernandezchaverri@gmail.com>
2021-01-17 20:07:23 -0600

    meora

--- src/main/java/com/emprendesoftcr/Bo/Impl/CertificadoBoImpl.java
+++ src/main/java/com/emprendesoftcr/Bo/Impl/CertificadoBoImpl.java
@@ -40,8 +40,8 @@ public class CertificadoBoImpl implements CertificadoBo {
 			// desarrollo
 			// ClassPathResource cpr = new ClassPathResource("/home/jose/Escritorio/llaves/" + nombrep12);
 
-			nombrep12 = "011085030031.p12";
-			password = "1694";
+			nombrep12 = "011255092337.p12";
+			password = "0923";
 			ClassPathResource cpr = new ClassPathResource("/home/jose/dev/llaves/produccion/" + nombrep12);
 			String path = new ClassPathResource("/home/jose/dev/llaves/tmp/").getPath();
 			certificateInfo = P12Utils.dataFromP12(path, cpr.getPath(), password);
