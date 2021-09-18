3e36287bae2c8985bd9ffb0e3711594e28c4ada3
josehernandezchaverri@gmail.com <josehernandezchaverri@gmail.com>
2021-09-17 22:16:08 -0600

    mejoras

--- src/main/java/com/emprendesoftcr/seguridad/WebSecurityConfig.java
+++ src/main/java/com/emprendesoftcr/seguridad/WebSecurityConfig.java
@@ -117,6 +117,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/login").permitAll()
 				.antMatchers("https://api.hacienda.go.cr/").permitAll()
 				.antMatchers("https://api.hacienda.go.cr/indicadores/tc").permitAll()
+				.antMatchers("https://api.hacienda.go.cr/fe/ae").permitAll()
 				.antMatchers("/service/callback.do").permitAll()
 				.antMatchers("/webjars/**").permitAll()
 				.antMatchers("/login")
